7ba8d0a286840c570e4ba10e901d29ffc784753b
Nanda Satria <satria354.nba@gmail.com>
2024-10-04 09:49:51 +0700

    add help options

--- main.go
+++ main.go
@@ -9,14 +9,33 @@ import (
 	"strings"
 )
 
+func printHelp() {
+	fmt.Println("Usage: code-reviewer [options]")
+	fmt.Println("Options:")
+	fmt.Println("  -scandir string")
+	fmt.Println("        Directory to scan (default \".\")")
+	fmt.Println("  -exclude string")
+	fmt.Println("        Comma-separated list of directories, files, extensions, or regex patterns to exclude")
+	fmt.Println("  -extensions string")
+	fmt.Println("        Comma-separated list of extensions used")
+	fmt.Println("  -keywords string")
+	fmt.Println("        Comma-separated list of keywords to filter files")
+}
+
 func main() {
 
 	scandir := flag.String("scandir", ".", "Directory to scan")
 	excludes := flag.String("exclude", "", "Comma-separated list of directories, files, extensions, or regex patterns to exclude")
 	extensions := flag.String("extensions", "", "Comma-separated list of extensions used")
 	keywords := flag.String("keywords", "", "Comma-separated list of keywords to filter files")
+	help := flag.Bool("help", false, "Show help")
 
 	flag.Parse()
+	if *help {
+		printHelp()
+		return
+	}
+
 	excludePatterns := strings.Split(*excludes, ",")
 	extensionList := strings.Split(*extensions, ",")
 	keywordList := strings.Split(*keywords, ",")
---
+++ readme.md
@@ -0,0 +1,20 @@
+# AI Code Reviewer
+
+## Prerequisite
+
+Prepare .env file with content:
+
+```config
+AZURE_OPENAI_ENDPOINT=https://xxx.openai.azure.com
+AZURE_OPENAI_KEY=
+AZURE_OPENAI_MODEL=
+AZURE_OPENAI_API_VERSION=
+```
+
+
+## How to use
+
+
+## Version 0.1
+
+- Supported AI Gen : Azure OpenAI
